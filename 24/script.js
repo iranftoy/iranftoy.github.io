3343cc590a58d1ea8e8f8e58a54791070fdc7605
iranftoy <103583783+iranftoy@users.noreply.github.com>
2025-04-14 09:19:32 +0800

    Add files via upload

--- 24/script.js
+++ 24/script.js
@@ -15,6 +15,30 @@ let gameStats = {
 // 初始化统计
 loadStats();
 
+function confirmScore() {
+    if(selectedPlayers.length === 0) {
+        updateScores(false,20)
+        showSolution();
+        startRound()
+    }
+    
+    const points = selectedPlayers.length === 3 ? [2,1,1] : 
+        selectedPlayers.length === 2 ? [2,2] : [4];
+    
+    selectedPlayers.forEach((p,i) => {
+        scores[p] += points[i] || 2;
+    });
+    
+    updateScores();
+    startRound();
+}
+
+function updateScores() {
+    scores.forEach((s,i) => {
+        document.getElementById(`score${i}`).textContent = s;
+    });
+}
+
 function calculateSolutions(nums) {
     const ops = ['+', '-', '*', '/'];
     
@@ -46,6 +70,68 @@ function calculateSolutions(nums) {
     return dfs([...nums]);
 }
 
+function valid() {
+    var s = arr;
+    if (s.length != 4) {
+        alert("必须输入4个整数！");
+        F.a.focus();
+        return false
+    }
+    //格式化输入
+    if (s.join(" ") != F.a.value) F.a.value = s.join(" ")
+    var b = [] //四个数字保存在b[1] b[2] b[4] b[8]中
+    for (var i in s) b[1 << i] = parseInt(s[i])
+    var n = F.count1.options[F.count1.selectedIndex].value; //需要计算的答案数
+    var k = 0 //已经算出的答案数
+    var result = {} //保存已经算出的答案,去掉重复
+    F.text.value = "";
+    //输出结果,找满答案返回true
+    var writeResult = function(s) {
+        //按顺序替换符号与数字
+        s = s.replace("%", O[f1].name).replace("%", O[f2].name).replace("%", O[f3].name)
+        s = s.replace("N", b[i1]).replace("N", b[i2]).replace("N", b[i3]).replace("N", b[i4])
+        if (result[s]) return //已经重复了
+        result[s] = 1 //记录
+        F.text.value += s + "\n";
+        return ++k >= n
+    }
+    for (var i1 = 1; i1 <= 8; i1 <<= 1)
+        for (var i2 = 1; i2 <= 8; i2 <<= 1)
+            for (var i3 = 1; i3 <= 8; i3 <<= 1)
+                for (var i4 = 1; i4 <= 8; i4 <<= 1) {
+                    //所有数字排列组合，简单去掉重复数字
+                    if ((i1 | i2 | i3 | i4) != 0xf) continue;
+                    for (var f1 = 0; f1 <= 3; f1++)
+                        for (var f2 = 0; f2 <= 3; f2++)
+                            for (var f3 = 0; f3 <= 3; f3++) {
+                                var of1 = O[f1],
+                                    of2 = O[f2],
+                                    of3 = O[f3];
+                                // ((1,2)3)4
+                                var m = of3.f(of2.f(of1.f(b[i1], b[i2]), b[i3]), b[i4]);
+                                if (Math.abs(m - 24) < 1e-5)
+                                    if (writeResult("((N%N)%N)%N")) return false
+                                // 1((2,3)4)
+                                m = of1.f(b[i1], of3.f(of2.f(b[i2], b[i3]), b[i4]));
+                                if (Math.abs(m - 24) < 1e-5)
+                                    if (writeResult("N%((N%N)%N)")) return false
+                                //  (1(2,3))4
+                                m = of3.f(of1.f(b[i1], of2.f(b[i2], b[i3])), b[i4]);
+                                if (Math.abs(m - 24) < 1e-5)
+                                    if (writeResult("(N%(N%N))%N")) return false
+                                //1(2(3,4))
+                                m = of1.f(b[i1], of2.f(b[i2], of3.f(b[i3], b[i4])));
+                                if (Math.abs(m - 24) < 1e-5)
+                                    if (writeResult("N%(N%(N%N))")) return false
+                                //(1,2)(3,4)
+                                m = of2.f(of1.f(b[i1], b[i2]), of3.f(b[i3], b[i4]));
+                                if (Math.abs(m - 24) < 1e-5)
+                                    if (writeResult("(N%N)%(N%N)")) return false
+                            }
+                }
+    F.text.value += "----END----\n"
+    return false
+}
 function generateNumbers() {
     const hasHigh = document.getElementById('highNumbers').checked;
     do {
@@ -89,7 +175,7 @@ document.addEventListener('keydown', (e) => {
             updateStats(true, timeUsed);
         } else {
             isPaused = false;
-            startRound();
+            confirmScore();
         }
     }
 
@@ -170,7 +256,7 @@ function updateScorePreview() {
 
 function showSolution() {
     document.getElementById('solution').textContent = 
-        `题目答案：${numbers.join(' ')} 可以组成24点`;
+        `Failed`;
 }
 
 // 初始化游戏
